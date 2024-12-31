5a4a6989d56c308c3f32fa8e7340c8a850ba9d65
Gungcakra <agungcakra888@gmail.com>
2024-12-31 18:09:53 +0800

    update logout redirection URL for cashflow environment

--- system/logout/index.php
+++ system/logout/index.php
@@ -7,8 +7,8 @@ session_destroy();
 
 if ($_SERVER['HTTP_HOST'] === 'localhost') {
     header("Location: " . BASE_URL_HTML);
-} else if ($_SERVER['HTTP_HOST'] === 'php-template.cakra-portfolio.my.id') {
-    header("Location: https://php-template.cakra-portfolio.my.id/");
+} else if ($_SERVER['HTTP_HOST'] === 'cashflow.cakra-portfolio.my.id') {
+    header("Location: https://cashflow.cakra-portfolio.my.id/");
 }
 // if (isset($_GET['id']) && base64_decode($_GET['id']) === session_id()) {
 
