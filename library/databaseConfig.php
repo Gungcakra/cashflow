184d0f068aaecc633380670d5b980ac476218f0b
Gungcakra <agungcakra888@gmail.com>
2025-01-05 11:14:45 +0800

    update .gitignore

--- library/databaseConfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if ($_SERVER['HTTP_HOST'] === 'localhost') {
-    $db = mysqli_connect("localhost", "root", "", "cashflowdb");
-} else if ($_SERVER['HTTP_HOST'] === 'cashflow.cakra-portfolio.my.id') {
-    $db = mysqli_connect("localhost", "u686303384_cashflowdb", "#Cashflow12", "u686303384_cashflowdb");
-}
